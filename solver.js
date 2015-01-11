f379951a76d293f62e8ac7bdfdd370b50120218e
Clinton <iblamefish@gmail.com>
2015-01-11 22:30:48 +0000

    add frequency analysis and horizontal word detection

--- solver.js
+++ solver.js
@@ -18,15 +18,53 @@
     solve(data);
   });
 
+  function frequency_analysis(arr) {
+    return _(arr).reduce(function(acc, letter) {
+      if (!isNaN(letter)) {
+        if (typeof acc[letter] == "undefined") {
+          acc[letter] = 0;
+        }
+        acc[letter] ++;
+      }
+      return acc;
+    }, {});
+  }
+
+  function horizontal_word_list(grid) {
+    return _.chain(grid).map(function(row) {
+      return _.chain(row).reduce(function(wordlist, letter) {
+        var index = wordlist.length === 0 ? 0 : wordlist.length - 1;
+        if (!isNaN(letter)) {
+          if (typeof wordlist[index] == "undefined") {
+            wordlist[index] = [];
+          }
+          wordlist[index].push(letter);
+        } else {
+          wordlist.push([]);
+        }
+        return wordlist;
+      }, []).filter(function(word) {
+        return word.length > 1;
+      }).value();
+    }).filter(function(row) {
+      return row.length > 0;
+    }).flatten(true).value();
+  }
+
   function solve(data) {
-    var grid = processGrid(data);
+    var grid = processGrid(data),
+      letter_frequency,
+      words;
 
     if (!validGrid(grid)) {
-      console.log("Grid is not valid");
+      console.log("Grid is not valid (check that each row has the same number of cells");
       return;
     }
 
-    console.log(grid);
+    letter_frequency = frequency_analysis(_(grid).flatten());
+    words = horizontal_word_list(grid);
+
+    console.log(words);
   }
 
   function processGrid(grid) {
